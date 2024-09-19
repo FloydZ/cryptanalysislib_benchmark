49ba15099f8db5b9810158ff2bfad1040004db85
= <zweydfg8+github@rub.de>
2024-09-19 20:34:19 +0200

    gitbak

--- bench/hashmap/simple.cpp
+++ bench/hashmap/simple.cpp
@@ -1,41 +1,84 @@
 #include <benchmark/benchmark.h>
-
-#include "random.h"
-#include "container/hashmap.h"
 #include "absl/container/flat_hash_map.h"
+
+#include "container/hashmap.h"
+#include "container/imap.h"
 
 constexpr size_t LS = 1u << 12u;
 
 template<typename T>
 static void BM_abseil_flat_hash_map(benchmark::State &state) {
-	static std::vector<T> data;
-	absl::flat_hash_map<int, std::string> map1;
+    absl::flat_hash_map<T, T> map;
 
-	for (auto _: state) {
-		for (size_t i = 0; i < data.size(); ++i) {
-			cnt[data[i]] += 1;
-		}
-		benchmark::ClobberMemory();
-	}
+    for (auto _: state) {
+        for (int64_t i = 0; i < state.range(0); ++i) {
+            map[i] = i;
+        }
+        benchmark::ClobberMemory();
+
+        for (int64_t i = 0; i < state.range(0); ++i) {
+            auto t = map.find(i);
+            (void)t;
+        }
+        benchmark::ClobberMemory();
+
+        for (int64_t i = 0; i < state.range(0); ++i) {
+            map.erase(i);
+        }
+        benchmark::ClobberMemory();
+    }
 }
+BENCHMARK(BM_abseil_flat_hash_map<uint32_t>)->RangeMultiplier(2)->Range(32, LS);
+BENCHMARK(BM_abseil_flat_hash_map<uint64_t>)->RangeMultiplier(2)->Range(32, LS);
 
-
-#ifdef USE_AVX2
 template<typename T>
-static void BM_histogram_avx2(benchmark::State &state) {
-	static std::vector<T> data;
-	generate_data(data, state.range(0));
-	for (auto i = 0; i < state.range(0); ++i) { data[i] = data[i] & 0xFF; }
-
+static void BM_std_unordered_map(benchmark::State &state) {
+	std::unordered_map<T, T> map;
 
 	for (auto _: state) {
-		avx2_histogram_u32(cnt, data.data(), state.range(0));
+		for (int64_t i = 0; i < state.range(0); ++i) {
+            map[i] = i;
+		}
+        benchmark::ClobberMemory();
+
+		for (int64_t i = 0; i < state.range(0); ++i) {
+            auto t = map.find(i);
+            (void)t;
+        }
+        benchmark::ClobberMemory();
+
+        for (int64_t i = 0; i < state.range(0); ++i) {
+             map.erase(i);
+        }
 		benchmark::ClobberMemory();
 	}
 }
-BENCHMARK(BM_histogram_avx2<uint32_t>)->RangeMultiplier(2)->Range(32, LS);
-#endif
+BENCHMARK(BM_std_unordered_map<uint32_t>)->RangeMultiplier(2)->Range(32, LS);
+BENCHMARK(BM_std_unordered_map<uint64_t>)->RangeMultiplier(2)->Range(32, LS);
 
-BENCHMARK(BM_stupid_histogram<uint8_t>)->RangeMultiplier(2)->Range(32, LS);
+static void BM_imap(benchmark::State &state) {
+    imap_tree_t tree(state.range(0));
+    uint32_t *slot;
+    for (auto _: state) {
+        for (int64_t i = 0; i < state.range(0); ++i) {
+            slot = tree.assign(i);
+            (void)slot;
+        }
+        benchmark::ClobberMemory();
+
+        for (int64_t i = 0; i < state.range(0); ++i) {
+            slot = tree.lookup(i);
+            (void)slot;
+        }
+        benchmark::ClobberMemory();
+
+        for (int64_t i = 0; i < state.range(0); ++i) {
+            tree.remove(i);
+        }
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_imap)->RangeMultiplier(2)->Range(32, LS);
+
 
 BENCHMARK_MAIN();
