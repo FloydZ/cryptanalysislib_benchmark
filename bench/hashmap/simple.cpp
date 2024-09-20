21004931dd1617faf701946e3603502f6d8e0781
= <zweydfg8+github@rub.de>
2024-09-20 08:46:34 +0200

    gitbak

--- bench/hashmap/simple.cpp
+++ bench/hashmap/simple.cpp
@@ -1,14 +1,21 @@
 #include <benchmark/benchmark.h>
 #include "absl/container/flat_hash_map.h"
+#include "absl/container/node_hash_map.h"
 
 #include "container/hashmap.h"
 #include "container/imap.h"
 
 constexpr size_t LS = 1u << 12u;
 
-template<typename T>
-static void BM_abseil_flat_hash_map(benchmark::State &state) {
-    absl::flat_hash_map<T, T> map;
+template <class Key, class Val>
+using abseil_node_hash_map = absl::node_hash_map<Key, Val>;
+template <class Key, class Val>
+using abseil_flat_hash_map = absl::flat_hash_map<Key, Val>;
+
+template<typename Map>
+static void BM_std_map(benchmark::State &state) {
+    Map map;
+    map.reserve(state.range(0));
 
     for (auto _: state) {
         for (int64_t i = 0; i < state.range(0); ++i) {
@@ -28,33 +35,14 @@ static void BM_abseil_flat_hash_map(benchmark::State &state) {
         benchmark::ClobberMemory();
     }
 }
-BENCHMARK(BM_abseil_flat_hash_map<uint32_t>)->RangeMultiplier(2)->Range(32, LS);
-BENCHMARK(BM_abseil_flat_hash_map<uint64_t>)->RangeMultiplier(2)->Range(32, LS);
 
-template<typename T>
-static void BM_std_unordered_map(benchmark::State &state) {
-	std::unordered_map<T, T> map;
+BENCHMARK(BM_std_map<absl::flat_hash_map<uint32_t, uint32_t>>)->RangeMultiplier(2)->Range(32, LS);
+BENCHMARK(BM_std_map<absl::flat_hash_map<uint64_t, uint64_t>>)->RangeMultiplier(2)->Range(32, LS);
+BENCHMARK(BM_std_map<absl::node_hash_map<uint32_t, uint32_t>>)->RangeMultiplier(2)->Range(32, LS);
+BENCHMARK(BM_std_map<absl::node_hash_map<uint64_t, uint64_t>>)->RangeMultiplier(2)->Range(32, LS);
+BENCHMARK(BM_std_map<std::unordered_map<uint32_t, uint32_t>>)->RangeMultiplier(2)->Range(32, LS);
+BENCHMARK(BM_std_map<std::unordered_map<uint64_t, uint64_t>>)->RangeMultiplier(2)->Range(32, LS);
 
-	for (auto _: state) {
-		for (int64_t i = 0; i < state.range(0); ++i) {
-            map[i] = i;
-		}
-        benchmark::ClobberMemory();
-
-		for (int64_t i = 0; i < state.range(0); ++i) {
-            auto t = map.find(i);
-            (void)t;
-        }
-        benchmark::ClobberMemory();
-
-        for (int64_t i = 0; i < state.range(0); ++i) {
-             map.erase(i);
-        }
-		benchmark::ClobberMemory();
-	}
-}
-BENCHMARK(BM_std_unordered_map<uint32_t>)->RangeMultiplier(2)->Range(32, LS);
-BENCHMARK(BM_std_unordered_map<uint64_t>)->RangeMultiplier(2)->Range(32, LS);
 
 static void BM_imap(benchmark::State &state) {
     imap_tree_t tree(state.range(0));
